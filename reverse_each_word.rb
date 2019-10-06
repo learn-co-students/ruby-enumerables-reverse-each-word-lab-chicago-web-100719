e3c493f0fe11213fffa1e435a2057a884ff41705
Benjamin Davis <bennyadave@gmail.com>
2019-10-06 03:02:37 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,5 @@
 def reverse_each_word(string)
   array = string.split(" ")
-  new_array = []
-  array.collect { |word| new_array.push(word.reverse) }
+  new_array = array.collect { |word| word.reverse }
   return new_array.join(" ")
 end 
